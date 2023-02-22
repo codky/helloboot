d31f54f0c494cf490970aa1ae1e99308c7e41d61
codky <msky0912@gmail.com>
2023-02-22 18:22:10 +0900

    dispatcherServlet

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,14 +1,24 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Objects;
 
-public class HelloController {
-    public String hello(String name) {
-        SimpleHelloService helloService = new SimpleHelloService();
 
+@RequestMapping("/hello")
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping// 예전에는 @RequestMapping
+    @ResponseBody // @RestController 를 위에 붙였다면 모든 메소드에 @ResponseBody가 붙었다고 가정한다.
+    public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
