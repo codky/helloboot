c4678f0f709ae16613fccf4fc392602cf077d582
msk <msky0912@gmail.com>
2023-03-13 23:29:14 +0900

    repository를 사용하는 HelloService

--- src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -11,11 +11,6 @@ public class HelloRepositoryTest {
     @Autowired JdbcTemplate jdbcTemplate;
     @Autowired HelloRepository helloRepository;
 
-    @BeforeEach
-    void init() {
-        jdbcTemplate.execute("create table if not exists hello(name varchar(50) primary key, count int)");
-    }
-
     @Test
     void findHelloFailed() { // 실패 테스트
         Assertions.assertThat(helloRepository.findHello("Toby")).isNull();
